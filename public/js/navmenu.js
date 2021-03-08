579e3313bd34a938d909b5f7c66a5639c300a357
karuifeather <aryalaashaya@gmail.com>
2021-03-08 08:25:03 +0545

    fixes + responsive nav complete

--- public/js/navmenu.js
+++ public/js/navmenu.js
@@ -2,5 +2,17 @@ const $navham = document.querySelector('.nav__ham');
 
 $navham.addEventListener('click', () => {
   $navham.classList.toggle('is-active');
-  console.log('clicked');
+  document.getElementById($navham.dataset.target).classList.toggle('is-active');
+});
+
+document.addEventListener('click', (e) => {
+  if (
+    e.target.parentNode !== $navham &&
+    $navham.classList.contains('is-active')
+  ) {
+    $navham.classList.toggle('is-active');
+    document
+      .getElementById($navham.dataset.target)
+      .classList.toggle('is-active');
+  }
 });
