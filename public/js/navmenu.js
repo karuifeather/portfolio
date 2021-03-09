e1db7685cecc696870c6d6d773a71eece92417f2
karuifeather <aryalaashaya@gmail.com>
2021-03-09 06:22:36 +0545

    closing nav when clicked outside; other glitches fixed

--- public/js/navmenu.js
+++ public/js/navmenu.js
@@ -6,13 +6,23 @@ $navham.addEventListener('click', () => {
 });
 
 document.addEventListener('click', (e) => {
-  if (
-    e.target.parentNode !== $navham &&
-    $navham.classList.contains('is-active')
-  ) {
-    $navham.classList.toggle('is-active');
+  if (e.target === $navham) return;
+
+  // check if the target is within navham
+  // i.e user clicks on span elements
+  let isItFalseAlaram = false;
+  $navham.querySelectorAll('.nav__ham-line').forEach((element) => {
+    if (e.target === element) {
+      isItFalseAlaram = true;
+    }
+  });
+
+  if (isItFalseAlaram) return;
+
+  if ($navham.classList.contains('is-active')) {
+    $navham.classList.remove('is-active');
     document
       .getElementById($navham.dataset.target)
-      .classList.toggle('is-active');
+      .classList.remove('is-active');
   }
 });
