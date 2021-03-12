1d323a90bbcc942b683929c7ebddfb0321995e48
karuifeather <aryalaashaya@gmail.com>
2021-03-12 11:02:51 +0545

    todo: hide nav when not scrolling

--- public/js/stickyNav.js
+++ public/js/stickyNav.js
@@ -1,6 +1,19 @@
 const $nav = document.querySelector('.nav');
 
+// let previousScrollTop = 0;
 document.addEventListener('scroll', () => {
   const { scrollTop } = document.documentElement;
   $nav.classList.toggle('is-sticky', scrollTop > 95);
+
+  // if (scrollTop === 0) {
+  //   $nav.classList.remove('is-sticky');
+  //   return;
+  // }
+
+  // if (scrollTop < previousScrollTop) {
+  //   $nav.classList.add('is-sticky');
+  // } else {
+  //   $nav.classList.remove('is-sticky');
+  // }
+  // previousScrollTop = scrollTop;
 });
